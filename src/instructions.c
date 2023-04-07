a2b96af6dfe2180431c1f5ce946e2365a88b8210
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-04-06 23:12:53 -0300

    Added addresssing mode enum

--- src/instructions.c
+++ src/instructions.c
@@ -47,3 +47,30 @@ void update_zero_and_negative_flags(CPU *cpu, uint8_t result)
         cpu->status = cpu->status & !NEGATIVE_FLAG;
     }
 }
+
+uint16_t get_operand_addr(CPU *cpu, AddrMode mode)
+{
+    switch (mode)
+    {
+        case Immediate: return cpu->program_counter;
+
+        case ZeroPage: return read_mem(cpu, cpu->program_counter);
+        case ZeroPageX: return read_mem(cpu, cpu->program_counter + cpu->reg_x);
+        case ZeroPageY: return read_mem(cpu, cpu->program_counter + cpu->reg_y);
+
+        case Absolute: return read_mem_u16(cpu, cpu->program_counter);
+        case AbsoluteX: return read_mem_u16(cpu, cpu->program_counter) + cpu->reg_x;
+        case AbsoluteY: return read_mem_u16(cpu, cpu->program_counter) + cpu->reg_y;
+
+        case Indirect:
+            uint16_t base = read_mem_u16(cpu, cpu->program_counter);
+            return read_mem_u16(cpu, base);
+        case IndirectX:
+            uint8_t base = read_mem(cpu, cpu->program_counter);
+            return read_mem_u16(cpu, base + cpu->reg_x);
+        case IndirectY:
+            uint16_t base = read_mem_u16(cpu, cpu->program_counter);
+            return base + cpu->reg_y;
+        case NoneAddressing: return;
+    }
+}
--- src/instructions.h
+++ src/instructions.h
@@ -4,6 +4,34 @@
 #include "cpu.h"
 #include <stdint.h>
 
+typedef enum
+{
+    Immediate,
+
+    ZeroPage,
+    ZeroPageX,
+    ZeroPageY,
+
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+
+    Indirect,
+    IndirectX,
+    IndirectY,
+
+    NoneAddressing
+} AddrMode;
+
+typedef struct
+{
+    uint8_t opcode;
+    char mnemonic[3];
+    int bytes;
+    int cycles;
+    AddrMode mode;
+} Instruction;
+
 // CPU flag bits
 #define NEGATIVE_FLAG 0b10000000
 #define ZERO_FLAG 0b00000010
@@ -13,5 +41,6 @@ void inx(CPU *cpu);
 void lda(CPU *cpu, uint8_t value);
 void tax(CPU *cpu);
 void update_zero_and_negative_flags(CPU *cpu, uint8_t result);
+uint16_t get_operand_addr(CPU *cpu, AddrMode mode);
 
 #endif
