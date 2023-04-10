00e546d5e68404166a4f8932d09d5375f1193b7d
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-04-10 11:09:14 -0300

    Added instruction function headers to instructions.h

--- src/instructions.h
+++ src/instructions.h
@@ -45,21 +45,109 @@ void populate_inst_list(void);
 Instruction get_instruction_from_opcode(uint8_t opcode);
 
 // Instructions
+// In alphabetical order
+
 void adc(CPU *cpu, AddrMode mode);
 void and(CPU *cpu, AddrMode mode);
 void asl_acc(CPU *cpu);
 void asl(CPU *cpu, AddrMode mode);
+
+// Branch instructions part 1
 uint8_t bcc(CPU *cpu);
 uint8_t bcs(CPU *cpu);
+uint8_t beq(CPU *cpu);
+
+void bit(CPU *cpu, AddrMode mode);
+
+// Branch instructions part 2
+uint8_t bmi(CPU *cpu);
+uint8_t bne(CPU *cpu);
+uint8_t bpl(CPU *cpu);
+
 void brk();
+
+// Branch instructions part 3
+uint8_t bvc(CPU *cpu);
+uint8_t bvs(CPU *cpu);
+
+// Clear instructions
+void clc(CPU *cpu);
+void cld(CPU *cpu);
+void cli(CPU *cpu);
+void clv(CPU *cpu);
+
+// Compare instructions
+void cmp(CPU *cpu, AddrMode mode);
+void cpx(CPU *cpu, AddrMode mode);
+void cpy(CPU *cpu, AddrMode mode);
+
+// Decrement instructions
+void dec(CPU *cpu, AddrMode mode);
+void dex(CPU *cpu);
+void dey(CPU *cpu);
+
+void eor(CPU *cpu, AddrMode mode);
+
+// Increment instructions
+void inc(CPU *cpu, AddrMode mode);
 void inx(CPU *cpu);
+void iny(CPU *cpu);
+
+// Jump instructions
+void jmp(CPU *cpu, AddrMode mode);
+void jsr(CPU *cpu);
+
+// Load instructions
 void lda(CPU *cpu, AddrMode mode);
+void ldx(CPU *cpu, AddrMode mode);
+void ldy(CPU *cpu, AddrMode mode);
+
+void lsr(CPU *cpu, AddrMode mode);
+void nop();
+void ora(CPU *cpu, AddrMode mode);
+
+// Push instructions
+void pha(CPU *cpu);
+void php(CPU *cpu);
+
+// Pull instructions
+void pla(CPU *cpu);
+void plp(CPU *cpu);
+
+// Rotate instructions
+void rol(CPU *cpu, AddrMode mode);
+void ror(CPU *cpu, AddrMode mode);
+
+// Return instructions
+void rti(CPU *cpu);
+void rts(CPU *cpu);
+
+void sbc(CPU *cpu, AddrMode mode);
+
+// Set flag instructions
+void sec(CPU *cpu);
+void sed(CPU *cpu);
+void sei(CPU *cpu);
+
+// Store accumulator
+void sta(CPU *cpu, AddrMode mode);
+void stx(CPU *cpu, AddrMode mode);
+void sty(CPU *cpu, AddrMode mode);
+
+// Transfer instructions
 void tax(CPU *cpu);
+void tay(CPU *cpu);
+void tsx(CPU *cpu);
+void txa(CPU *cpu);
+void txs(CPU *cpu);
+void tya(CPU *cpu);
 
 // Register functions
 void set_reg_a(CPU *cpu, uint8_t value);
 void set_reg_x(CPU *cpu, uint8_t value);
 void set_reg_y(CPU *cpu, uint8_t value);
+
+// End instructions
 
 // Utility functions
 void update_zero_and_negative_flags(CPU *cpu, uint8_t result);
